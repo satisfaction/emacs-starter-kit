90acd6bdce9cbe5a94d0f2f35793b0860ecbe3da
Pair <pair@darkside.local>
2011-08-22 09:03:55 -0700

    Set css indent level to 2

--- pair.el
+++ pair.el
@@ -1,6 +1,14 @@
 (push "/usr/local/bin" exec-path)
 ;; no more backup files emacs, I got it
 (setq make-backup-files nil)
+
+;; set the javascript indent level to the non-standard 2 spaces
+;; instead of 4
+(setq-default js-indent-level 2)
+
+;; the default css indent level is 4; set it to 2
+(setq css-indent-offset 2)
+
 ;; set font type and size
 (set-default-font "Monaco-9")
 ;; set new twilight theme

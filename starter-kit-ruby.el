97edf7719a40735a337bcee58822e08325b98a34
Piotr Biegaj <pbiegaj@PeteTheSadPandas-Bamboo-Emporium.local>
2011-09-13 16:20:16 -0700

    User rspec-mode instead of rpm-spec-mode and update those files extensions

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -18,10 +18,12 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("spec\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")

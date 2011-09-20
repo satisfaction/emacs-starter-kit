3f89d4e1e4d5e8a5e1f4e2938c9112a30d0b5fc8
Piotr Biegaj <piotr.biegaj@gmail.com>
2011-09-20 10:30:15 -0700

    More removal of rinari from my own personal config file

--- pbiegaj.el
+++ pbiegaj.el
@@ -10,20 +10,8 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/emacs-solarized")
 (require 'color-theme-solarized)
 (color-theme-solarized-dark)
-;; C-n and C-p when the autocomplete menu is up
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; (setq ac-use-quick-help t)
-;; (setq ac-use-menu-map t)
-;; (define-key ac-menu-map "\C-n" 'ac-next)
-;; (define-key ac-menu-map "\C-p" 'ac-previous)
-;; ;; smart case
-;; (setq ac-ignore-case 'smart)
+
 ;; rspec mode
 (add-to-list 'load-path "~/.emacs.d/vendor/rspec-mode")
 (require 'rspec-mode)
 (add-to-list 'auto-mode-alist '("spec\\.rb$" . ruby-mode))
-
-;; rinari minor mode
-;; (add-to-list 'load-path "~/.emacs.d/vendor/rinari")
-;; (require 'rinari)

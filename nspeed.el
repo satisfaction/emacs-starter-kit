98de90fda30fe366661ed553bd0a5e2aebf97642
Nathan Speed <nspeed@getsatisfaction.com>
2012-01-03 13:50:21 -0800

    Use sunburst emacs color theme in nspeed.el

--- nspeed.el
+++ nspeed.el
@@ -1,5 +1,10 @@
 ;; baseline defaults
 (load-file "~/.emacs.d/pair.el")
 
+;; my favorite theme
+(load-file "~/.emacs.d/vendor/Emacs-Sunburst-Color-Theme/color-theme-sunburst.el")
+;;(require 'color-theme-sunburst)
+(color-theme-sunburst)
+
 ;; use Deft
 (require 'deft)

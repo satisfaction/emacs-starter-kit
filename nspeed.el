2162f833b43aef95933e6c0bd35ce10762a03923
Nathan Speed <nspeed@getsatisfaction.com>
2012-02-11 00:01:59 -0800

    In nspeed.el, use different deft directories, depending on system-name

--- nspeed.el
+++ nspeed.el
@@ -2,9 +2,13 @@
 
 (install-and-initialize-packages
  '(
-   (deft . (lambda () (setq deft-directory "~/Dropbox/nathan_speed/notes/")))
+   (deft . (lambda ()
+             (setq my-deft-directory (if (string= system-name "switch") "~/Dropbox/gsfn/nathan_speed/notes/" "~/Dropbox/nathan_speed/notes/"))
+             (setq deft-directory my-deft-directory)
+             ))
    (pastels-on-dark-theme . no-op)
    )
  )
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
+

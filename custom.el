65bc168282f2338f63a1429540b9beb58a8a90a1
Nathan Speed <pair+nathan@getsatisfaction.com>
2012-06-29 16:01:56 -0700

    Flymake error highlighting: red background, black foreground. Certain syntax highlightings override the black foreground, but it is still pretty much readable.

--- custom.el
+++ custom.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flymake-errline ((((class color)) (:background "#ffffd7"))))
+ '(flymake-errline ((((class color)) (:background "Red" :foreground "Black"))))
  '(flymake-warnline ((((class color)) (:background "#0a2832")))))
 
 
